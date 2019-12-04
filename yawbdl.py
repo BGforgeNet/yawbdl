3864cca8fd6d4b568e4bb732b05613b49a23f902
Magus <magus@bgforge.net>
2019-12-04 13:23:48 +0700

    initial version

---
+++ yawbdl.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import requests
+from urllib.parse import urlparse
+from urllib.parse import urlsplit
+import sys
+import os
+import os.path as path
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Download a website from Internet Archive', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('-d', dest='domain', help='domain to download')
+parser.add_argument('-o', dest='dst_dir', help='output directory')
+parser.add_argument('--from', dest='from_date', default=None, action='append', help='from date, up to 14 digits: yyyyMMddhhmmss')
+parser.add_argument('--to', dest='to_date', default=None, help='to date')
+parser.add_argument('--timeout', dest='timeout', default=10, help='request timeout')
+
+args = parser.parse_args()
+
+if len(sys.argv) < 2:
+  parser.print_help(sys.stderr)
+  sys.exit(1)
+
+# init vars
+domain = args.domain
+dst_dir = args.dst_dir
+from_date = args.from_date
+to_date = args.to_date
+timeout = int(args.timeout)
+
+cdx_url = "http://web.archive.org/cdx/search/cdx?"
+params = "output=json&url={}&matchType=host&filter=statuscode:200&fl=timestamp,original".format(domain)
+if from_date is not None:
+  params = params + "&from={}".format(from_date)
+if to_date is not None:
+  params = params + "&to={}".format(to_date)
+
+vanilla_url = "http://web.archive.org/web/{}id_/{}"
+
+def get_snapshot_list():
+  resp = requests.get(cdx_url + params)
+  snap_list = resp.json()
+  del snap_list[0] # delete header
+  snap_list.sort(key = lambda row: row[0]) # sort by timestamp
+  return snap_list
+
+def download_files(snapshot_list):
+  total = len(snapshot_list)
+  i = 0
+  for snap in snapshot_list:
+    i += 1
+    print("({}/{}) ".format(i, total), end="")
+    download_file(snap)
+
+def get_file_path(original_url):
+  url = urlsplit(original_url)
+  fpath = url.path.lstrip('/')
+  if url.query:
+    fpath = fpath + "?" + url.query
+  if fpath.endswith('/') or fpath == "":
+    fpath = path.join(fpath, 'index.html')
+  return fpath
+
+def download_file(snap):
+  timestamp = snap[0]
+  original = snap[1]
+  print(timestamp, original, " ", end="", flush=True)
+
+  
+  fpath = path.join(dst_dir, timestamp, get_file_path(original))
+  if path.isfile(fpath):
+    print("[Already on disk, skipped].")
+    return
+
+  url = vanilla_url.format(timestamp, original)
+  resp = requests.get(url, timeout=timeout)
+  code = resp.status_code
+  if code != 200:
+    print("[Error: {}]".format(code), flush=True)
+  else:
+    content = resp.content
+    if len(content) == 0:
+      print("[File size is 0, not saving to disk]", flush=True)
+    else:
+      write_file(fpath, content)
+
+def write_file(fpath, content):
+  dirname, basename = path.split(fpath)
+  os.makedirs(dirname, exist_ok=True)
+  with open(fpath, "wb") as file:
+    file.write(content)
+  print("[OK]", flush=True)
+
+snap_list = get_snapshot_list()
+download_files(snap_list)
