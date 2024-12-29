762d27e956d0120898b0ca5efef48d3260976c89
Magus <magus@bgforge.net>
2024-12-30 00:12:15 +0700

    black autoformat

--- yawbdl.py
+++ yawbdl.py
@@ -11,24 +11,43 @@ import argparse
 import errno
 import time
 
-parser = argparse.ArgumentParser(description='Download a website from Internet Archive', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(
+    description="Download a website from Internet Archive",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
 
-parser.add_argument('-d', dest='domain', help='domain to download')
-parser.add_argument('-o', dest='dst_dir', help='output directory')
-parser.add_argument('--from', dest='from_date', default=None, help='from date, up to 14 digits: yyyyMMddhhmmss')
-parser.add_argument('--to', dest='to_date', default=None, help='to date')
-parser.add_argument('--timeout', dest='timeout', default=10, help='request timeout')
-parser.add_argument('-n', action='store_true', help="dry run")
-parser.add_argument('--delay', default=1, help="delay between requests")
-parser.add_argument('--retries', default=0, help="max number of retries")
-parser.add_argument('--no-fail', default=False, action='store_true', help="if retries are exceeded, and the file still couldn't have been downloaded, proceed to the next file instead of aborting the run")
-parser.add_argument('--skip-timestamps', default=None, action = 'append', nargs='+', help="skip snapshots with these timestamps (sometimes Internet Archive just fails to serve a specific snapshot)")
+parser.add_argument("-d", dest="domain", help="domain to download")
+parser.add_argument("-o", dest="dst_dir", help="output directory")
+parser.add_argument(
+    "--from",
+    dest="from_date",
+    default=None,
+    help="from date, up to 14 digits: yyyyMMddhhmmss",
+)
+parser.add_argument("--to", dest="to_date", default=None, help="to date")
+parser.add_argument("--timeout", dest="timeout", default=10, help="request timeout")
+parser.add_argument("-n", action="store_true", help="dry run")
+parser.add_argument("--delay", default=1, help="delay between requests")
+parser.add_argument("--retries", default=0, help="max number of retries")
+parser.add_argument(
+    "--no-fail",
+    default=False,
+    action="store_true",
+    help="if retries are exceeded, and the file still couldn't have been downloaded, proceed to the next file instead of aborting the run",
+)
+parser.add_argument(
+    "--skip-timestamps",
+    default=None,
+    action="append",
+    nargs="+",
+    help="skip snapshots with these timestamps (sometimes Internet Archive just fails to serve a specific snapshot)",
+)
 
 args = parser.parse_args()
 
 if len(sys.argv) < 2:
-  parser.print_help(sys.stderr)
-  sys.exit(1)
+    parser.print_help(sys.stderr)
+    sys.exit(1)
 
 # init vars
 domain = args.domain
@@ -41,128 +60,149 @@ delay = int(args.delay)
 retries = int(args.retries)
 no_fail = args.no_fail
 try:
-  skip_timestamps = args.skip_timestamps[0]
+    skip_timestamps = args.skip_timestamps[0]
 except:
-  skip_timestamps = []
+    skip_timestamps = []
 
 cdx_url = "http://web.archive.org/cdx/search/cdx?"
-params = "output=json&url={}&matchType=host&filter=statuscode:200&fl=timestamp,original".format(domain)
+params = "output=json&url={}&matchType=host&filter=statuscode:200&fl=timestamp,original".format(
+    domain
+)
 if from_date is not None:
-  params = params + "&from={}".format(from_date)
+    params = params + "&from={}".format(from_date)
 if to_date is not None:
-  params = params + "&to={}".format(to_date)
+    params = params + "&to={}".format(to_date)
 
 vanilla_url = "http://web.archive.org/web/{}id_/{}"
 
+
 def get_snapshot_list():
-  resp = requests.get(cdx_url + params)
-  snap_list = resp.json()
-  if len(snap_list) == 0:
-    print("Sorry, no snapshots found!")
-    sys.exit(0)
-  del snap_list[0] # delete header
-  snap_list.sort(key = lambda row: row[0]) # sort by timestamp
-  return snap_list
+    resp = requests.get(cdx_url + params)
+    snap_list = resp.json()
+    if len(snap_list) == 0:
+        print("Sorry, no snapshots found!")
+        sys.exit(0)
+    del snap_list[0]  # delete header
+    snap_list.sort(key=lambda row: row[0])  # sort by timestamp
+    return snap_list
+
 
 def download_files(snapshot_list):
-  total = len(snapshot_list)
-  i = 0
-  for snap in snapshot_list:
-    i += 1
-    print("({}/{}) ".format(i, total), end="")
-    download_file(snap)
+    total = len(snapshot_list)
+    i = 0
+    for snap in snapshot_list:
+        i += 1
+        print("({}/{}) ".format(i, total), end="")
+        download_file(snap)
+
 
 def get_file_path(original_url):
-  url = urlsplit(original_url)
-  fpath = url.path.lstrip('/')
+    url = urlsplit(original_url)
+    fpath = url.path.lstrip("/")
 
-  if url.query:
-    query_separator="?"
-    # Windows file paths can't contain "?". Use "@", like wget does.
-    if os.name == 'nt':
-      query_separator = "@"
-    fpath = fpath + query_separator + url.query
+    if url.query:
+        query_separator = "?"
+        # Windows file paths can't contain "?". Use "@", like wget does.
+        if os.name == "nt":
+            query_separator = "@"
+        fpath = fpath + query_separator + url.query
 
-  if fpath.endswith('/') or fpath == "":
-    fpath = path.join(fpath, 'index.html')
-  return fpath
+    if fpath.endswith("/") or fpath == "":
+        fpath = path.join(fpath, "index.html")
+    return fpath
+
 
 def download_file(snap):
-  timestamp = snap[0]
-  original = snap[1]
-  print(timestamp, original, " ", end="", flush=True)
+    timestamp = snap[0]
+    original = snap[1]
+    print(timestamp, original, " ", end="", flush=True)
 
-  if timestamp in skip_timestamps:
-    print("[Skip: by timestamp command line option]")
-    return
+    if timestamp in skip_timestamps:
+        print("[Skip: by timestamp command line option]")
+        return
 
-  fpath = path.join(dst_dir, timestamp, get_file_path(original))
-  if path.isfile(fpath):
-    print("[Skip: already on disk]")
-    return
+    fpath = path.join(dst_dir, timestamp, get_file_path(original))
+    if path.isfile(fpath):
+        print("[Skip: already on disk]")
+        return
 
-  if dry_run:
-    print("") # carriage return
-  else:
-    retry_count = 0
-    url = vanilla_url.format(timestamp, original)
-    while retry_count <= retries:
-      try:
-        if delay:
-          time.sleep(delay * 2 * retry_count) # increase delay with each try
-        resp = requests.get(url, timeout=timeout)
-        break
-      except Exception:
-        if retry_count < retries:
-          retry_count += 1
-          new_delay = delay * 2* retry_count
-          print("    failed to download, retrying after {} seconds... ".format(new_delay), flush=True)
-        else:
-          if no_fail:
-            print("    failed to download, proceeding to next file", flush=True)
-            return
-          else:
-            print("    failed to download, aborting", flush=True)
-            sys.exit(1)
-
-    code = resp.status_code
-    if code != 200:
-      print("[Error: {}]".format(code), flush=True)
+    if dry_run:
+        print("")  # carriage return
     else:
-      content = resp.content
-      if len(content) == 0:
-        print("[Skip: file size is 0]", flush=True)
-      else:
-        write_file(fpath, content)
+        retry_count = 0
+        url = vanilla_url.format(timestamp, original)
+        while retry_count <= retries:
+            try:
+                if delay:
+                    time.sleep(delay * 2 * retry_count)  # increase delay with each try
+                resp = requests.get(url, timeout=timeout)
+                break
+            except Exception:
+                if retry_count < retries:
+                    retry_count += 1
+                    new_delay = delay * 2 * retry_count
+                    print(
+                        "    failed to download, retrying after {} seconds... ".format(
+                            new_delay
+                        ),
+                        flush=True,
+                    )
+                else:
+                    if no_fail:
+                        print(
+                            "    failed to download, proceeding to next file",
+                            flush=True,
+                        )
+                        return
+                    else:
+                        print("    failed to download, aborting", flush=True)
+                        sys.exit(1)
+
+        code = resp.status_code
+        if code != 200:
+            print("[Error: {}]".format(code), flush=True)
+        else:
+            content = resp.content
+            if len(content) == 0:
+                print("[Skip: file size is 0]", flush=True)
+            else:
+                write_file(fpath, content)
+
 
 def write_file(fpath, content):
-  dirname, basename = path.split(fpath)
+    dirname, basename = path.split(fpath)
 
-  if path.isfile(dirname):
-    print("[Warning] file {} already exists, can't create directory with the same name for {}".format(dirname, basename), flush=True)
-    return
-  too_long = False
-  try:
-    os.makedirs(dirname, exist_ok=True)
-  except OSError as exc:
-    if exc.errno == errno.ENAMETOOLONG:
-      print("[Error: dir name too long, skipped]", flush=True)
-      too_long = True
-    else:
-      raise
-  if not too_long:
+    if path.isfile(dirname):
+        print(
+            "[Warning] file {} already exists, can't create directory with the same name for {}".format(
+                dirname, basename
+            ),
+            flush=True,
+        )
+        return
+    too_long = False
     try:
-      with open(fpath, "wb") as file:
-        file.write(content)
+        os.makedirs(dirname, exist_ok=True)
     except OSError as exc:
-      if exc.errno == errno.ENAMETOOLONG:
-        print("[Error: file name too long, skipped]", flush=True)
-        too_long = True
-      else:
-        raise
-    print("[OK]", flush=True)
+        if exc.errno == errno.ENAMETOOLONG:
+            print("[Error: dir name too long, skipped]", flush=True)
+            too_long = True
+        else:
+            raise
+    if not too_long:
+        try:
+            with open(fpath, "wb") as file:
+                file.write(content)
+        except OSError as exc:
+            if exc.errno == errno.ENAMETOOLONG:
+                print("[Error: file name too long, skipped]", flush=True)
+                too_long = True
+            else:
+                raise
+        print("[OK]", flush=True)
+
 
 snap_list = get_snapshot_list()
 download_files(snap_list)
 if dry_run:
-  print("Dry run completed.")
+    print("Dry run completed.")
